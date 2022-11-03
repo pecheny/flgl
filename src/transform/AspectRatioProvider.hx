8578a8b646ab556286bf8ebf1bb634ed72ce74a6
artem pecheny <pecheny@gmail.com>
2022-11-03 21:10:23 +0300

    Minor: aspect ratio type alias.

--- src/transform/AspectRatioProvider.hx
+++ src/transform/AspectRatioProvider.hx
@@ -1,6 +1,6 @@
 package transform;
 import Axis2D.ReadOnlyAVector2D;
 interface AspectRatioProvider {
-    public function getFactorsRef():ReadOnlyAVector2D<Float>;
+    public function getFactorsRef():AspectRatio;
 }
 
