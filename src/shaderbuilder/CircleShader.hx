03a72698ab98a6b183b72370a580a13213537501
artem pecheny <pecheny@gmail.com>
2024-10-08 20:27:11 +0300

    Finish and cleanup CircleShader.

--- src/shaderbuilder/CircleShader.hx
+++ src/shaderbuilder/CircleShader.hx
@@ -6,7 +6,7 @@ import data.aliases.VaryingAliases;
 class CircleShader implements ShaderElement {
     public static var instance = new CircleShader();
 
-    static var uv_v = VaryingAliases.UV_0;
+    static var uv = VaryingAliases.UV_0;
     static var r1 = CircleSet.R1_OUT;
     static var r2 = CircleSet.R2_OUT;
     static var aa = CircleSet.AASIZE;
@@ -14,15 +14,15 @@ class CircleShader implements ShaderElement {
     function new() {}
 // aa - thickness of antialiasing zone. sqrt(r) - empirical multiplier to make antialiasing visually similar
     public function getDecls():String {
-        return ' varying vec2 $uv_v; 
+        return ' varying vec2 $uv; 
                 varying float $aa;
                 varying float $r1;
                 varying float $r2;
 
-                float circle(in vec2 _st, in float r){
-                    vec2 dist = _st - vec2(0.5);
-                    return 1. -smoothstep(r - ($aa * sqrt(r)),
-                                        r + ($aa * sqrt(r)),
+                float circle(in float r){
+                    vec2 dist = $uv - vec2(0.5);
+                    float edge = sqrt(r) * $aa;
+                    return 1. -smoothstep(r - edge, r + edge,
                                         dot(dist, dist) * 4.0);
                 }
         ';
@@ -31,9 +31,8 @@ class CircleShader implements ShaderElement {
 
     public function getExprs():String {
         return '
-        vec2 st = $uv_v.xy ;
-        vec3 color = vec3(circle(st, $r2) * (1. - (circle(st, $r1))));;
-        gl_FragColor = vec4( color, 1.0 );
+            float val = circle($r2) * (1. - circle($r1));
+            gl_FragColor = vec4(1., 1., 1., val);
         ';
 
     }
