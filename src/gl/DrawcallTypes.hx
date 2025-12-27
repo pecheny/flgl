f7a45770198f4fb72a616dddd898cfe810bc253c
artem pecheny <pecheny@gmail.com>
2025-12-27 18:56:42 +0300

    DrawcallTypes.

---
+++ src/gl/DrawcallTypes.hx
@@ -0,0 +1,8 @@
+package gl;
+
+class PictureDrawcalls {
+    public static inline var IMAGE_DRAWCALL = "image";
+
+    public static function DRAWCALLS_LAYOUT(filename)
+        return '<drawcall type="image" font="" path="$filename" />';
+}
